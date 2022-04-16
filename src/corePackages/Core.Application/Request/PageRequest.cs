3bbe509d4586658dabe82ed3587fe4547829f0b1
melike yılmaz <melike_ylmz_61@hotmail.com>
2022-04-16 17:35:09 +0300

    fix error

--- src/corePackages/Core.Application/Request/PageRequest.cs
+++ src/corePackages/Core.Application/Request/PageRequest.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace Core.Application.Request
 {
+
     public class PageRequest
     {
 
         public int Page { get; set; }
         public int PageSize { get; set; }
     }
+
 }
